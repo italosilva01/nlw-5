1dfd84ab72ea5ebe5015986e0a320e1a4414d158
Francisco Italo Silva do Nascimento <italo.silvan87@gmail.com>
2021-04-21 07:05:34 -0300

    aula#02 terminada

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+//Salva as configurações do html, como formatação das páginas e fonts
+//É chamado apenas uma vez durante o ciclo de vida da aplicação
+import Document, { Html, Head, NextScript, Main } from "next/document";
+
+export default class myDocument extends Document {
+  render() {
+    return (
+      <Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Head>
+    );
+  }
+}
